2408f52be7ecb47d49e897252d109afa703cafcd
HaniifAJ <haniif02aj@gmail.com>
2023-07-23 14:24:10 +0700

    test

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package sdktest
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
-func printTest() {
-	fmt.Println("Hello World from SDK")
+func printTest(msg string) {
+	fmt.Println("Hello World from SDK:", msg)
+}
+
+func logInfo(msg string){
+	log.Printf("Info: %v", msg)
 }
